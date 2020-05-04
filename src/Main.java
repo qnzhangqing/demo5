9880d12ca565c95ffbf2e9fa3a758b6718f10f87
qnzhangqing <qnzhangqing@163.com>
2020-05-04 10:34:31 +0800

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @Projectname：demo5
+ * @Classname：Main
+ * @Description：TODO
+ * @CreateDate：2020/5/4 10:33
+ * @Author：zhangqingfeng
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ddd");
+    }
+}
